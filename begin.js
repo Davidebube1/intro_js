ab43c76b1e1ea13c4b69ba0a7251dfa361daee81
Davidebube1 <Davidebube1@users.noreply.github.com>
2022-06-21 18:54:31 +0100

    first javascript task

---
+++ begin.js
@@ -0,0 +1,10 @@
+// let name = "Okpodoko David Ebube";
+// let height = "6 feets";
+// let country = "Nigeria";
+
+
+alert("Name : Okpodoko David");
+alert("Height: 6ft 2in");
+alert("Country: Nigeria");
+ 
+
